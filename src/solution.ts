c0e5fde668770c526c4bf0764a0df3d5659842a2
faruqsoft <omarfaruqngdc@gmail.com>
2025-11-18 16:03:26 +0600

    problem 2 and 4 solve

--- src/solution.ts
+++ src/solution.ts
@@ -9,6 +9,9 @@ function formatValue(value: string | number | boolean): string | number | boolea
     }
 }
 
+
+
+
 function getLength (value : string | any[]) : number{
     if(typeof value === "string"){
         return value.length
@@ -18,4 +21,44 @@ function getLength (value : string | any[]) : number{
         return 0
     }    
 }
+
+
+
+class Person {
+    name : string;
+    age : number;
+
+    constructor (name : string, age : number){
+        this.name = name;
+        this.age = age;
+    }
+    getDetails():string{
+        return `'Name: ${this.name}, Age: ${this.age}';`;
+    }
+}
+
+const person1 = new Person('John Doe', 30);
+
+const person2 = new Person('Alice', 25);
+
+
+
+
+
+type Item = {
+    title : string;
+    rating : number;
+}
+function filterByRating (items:Item[]) : Item[]{
+    for(const item of items){
+        if (item.rating < 0 || item.rating >5 ){
+             throw new Error(`Invalid rating for "${item.title}". Rating must be between 0 and 5.`);
+        }
+    }
+    return items.filter(item => item.rating >= 4);
+}
+
+
+
+
 
