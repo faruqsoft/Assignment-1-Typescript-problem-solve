39a1b95dda9798b07f0b8920f993c4c4a81c0bb9
faruqsoft <omarfaruqngdc@gmail.com>
2025-11-18 15:08:25 +0600

    2 problem solve

---
+++ src/solution.ts
@@ -0,0 +1,21 @@
+
+function formatValue(value: string | number | boolean): string | number | boolean {
+    if (typeof value === "string") {
+        return value.toUpperCase();
+    }else if( typeof value === "number"){
+        return value*10
+    }else{
+        return !value
+    }
+}
+
+function getLength (value : string | any[]) : number{
+    if(typeof value === "string"){
+        return value.length
+    }else if (Array.isArray(value)){
+        return value.length
+    }else {
+        return 0
+    }    
+}
+
